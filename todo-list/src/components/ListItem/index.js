f49c903e543c12e2b10ce2d40b0b3f80f289f99f
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-16 05:06:09 -0300

    ListItem component

--- todo-list/src/components/ListItem/index.js
+++ todo-list/src/components/ListItem/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const ListItems = ({ stateItems, deleteItemHandler }) => {
+const ListItems = ({ stateItems, deleteItemHandler, setUpdate }) => {
   const listItems = stateItems.map((item) => {
     return (
       <div className="list" key={item.key}>
         <p>
-          {item.text}
+          {/* {item.text} */}
+          <input
+            type="text"
+            id={item.key}
+            value={item.text}
+            onChange={(e) => {
+              setUpdate(e.target.value, item.key);
+            }}
+          />
           <span>
             <FontAwesomeIcon
               className="faicons"
