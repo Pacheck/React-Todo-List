3fe9af13cf1448886ca52a18f7f3f6a50f6d6338
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-16 05:06:23 -0300

    App css / App js edit

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,77 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import ListItem from './components/ListItem';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 const App = () => {
-  const [items, setItems] = useState();
+  library.add(faTrash);
+
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({
+    text: '',
+    key: '',
+  });
+
+  function handleInput(e) {
+    setCurrentItem({
+      text: e.target.value,
+      key: Date.now(),
+    });
+  }
+
+  function addItem(e) {
+    e.preventDefault();
+    const newItem = currentItem;
+    if (newItem.text !== '') {
+      const newItems = [...items, newItem];
+
+      setItems(newItems);
+      setCurrentItem({
+        text: '',
+        key: '',
+      });
+    }
+  }
+
+  function deleteItemHandler(key) {
+    const filteredItems = items.filter((item) => item.key !== key);
+    setItems(filteredItems);
+  }
+
+  function setUpdate(text, key) {
+    const ITEMS = items;
+    ITEMS.map((item) => {
+      if (item.key === key) {
+        return (item.text = text);
+      }
+    });
+    console.log(ITEMS);
+    setItems(ITEMS);
+    setCurrentItem({ text: '', key: '' });
+  }
 
   return (
     <div className="App">
       <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Enter a task" />
+        <form id="to-do-form" onSubmit={addItem}>
+          <input
+            type="text"
+            placeholder="Enter a task"
+            value={currentItem.text}
+            onChange={handleInput}
+          />
           <button type="submit">Add</button>
         </form>
       </header>
+
+      <ListItem
+        stateItems={items}
+        deleteItemHandler={deleteItemHandler}
+        setUpdate={setUpdate}
+      />
     </div>
   );
 };
